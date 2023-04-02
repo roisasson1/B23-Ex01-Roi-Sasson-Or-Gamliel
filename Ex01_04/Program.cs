bcb919d63e26db4771cda7973c01d2deda3da208
Roi Sasson <84803473+roisasson1@users.noreply.github.com>
2023-04-02 21:55:57 +0300

    Update Program.cs

--- Ex01_04/Program.cs
+++ Ex01_04/Program.cs
@@ -14,7 +14,8 @@ public class Program
         Console.WriteLine("Hello, please enter a 6 character string:");
         string userStr = ReadStrFromUser(out bool isANumber);
         StringBuilder message = new StringBuilder();
-        message.AppendLine($"The string is {(IsPalindrome(userStr) ? "" : "not ")}a palindrom.");
+        message.AppendLine($"The string is {(IsPalindrom(userStr) ? "" : "not ")}a palindrom.");
+
         if (isANumber)
         {
             message.AppendLine($"The number is {(IsDividedBy3(userStr) ? "" : "not ")}divided by 3.");
@@ -24,10 +25,11 @@ public class Program
             int numOfUpperLetters = CountUpperLetters(userStr);
             message.AppendFormat("The word has {0} upper letter{1}.\n", numOfUpperLetters, numOfUpperLetters > 1 ? "s" : "");
         }
+
         return message;
     }
 
-    public static bool IsPalindrome(string i_usreStr)
+    public static bool IsPalindrom(string i_usreStr)
     {
         if (i_usreStr.Length == 0 || i_usreStr.Length == 1)
         {
@@ -41,15 +43,18 @@ public class Program
             }
             else
             {
-                return IsPalindrome(i_usreStr.Substring(1, i_usreStr.Length - 2));
+                return IsPalindrom(i_usreStr.Substring(1, i_usreStr.Length - 2));
             }
         }
     }
+
     public static bool IsDividedBy3(string i_usreStr)
     {
         int.TryParse(i_usreStr, out int num);
+
         return num % 3 == 0;
     }
+
     public static int CountUpperLetters(string i_userStr)
     {
         int numOfUpperLetters = 0;
@@ -58,8 +63,10 @@ public class Program
         {
             if (char.IsUpper(i_userStr[i])) numOfUpperLetters++;
         }
+
         return numOfUpperLetters;
     }
+
     public static string ReadStrFromUser(out bool io_isANumber)
     {
         string userStr = Console.ReadLine();
@@ -69,13 +76,16 @@ public class Program
             System.Console.WriteLine("The string is invalid! try again:");
             userStr = System.Console.ReadLine();
         }
+
         return userStr;
     }
+
     public static bool IsValid(string i_str, out bool io_isNumber)
     {
         io_isNumber = i_str.All(char.IsDigit);
         bool isAllLetters = i_str.All(char.IsLetter);
         bool valid = ((io_isNumber || isAllLetters) && i_str.Length == 6);
+
         return valid;
     }
 }
